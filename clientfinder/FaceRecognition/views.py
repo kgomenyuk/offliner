c3f45f576fd06b0af547ebf80fc9d5c1569e6887
Григорий Звездин <gazvezdin@edu.hse.ru>
2020-07-20 23:42:14 +0300

    dummy function age_gender_detection connected. try-except added

--- clientfinder/FaceRecognition/views.py
+++ clientfinder/FaceRecognition/views.py
@@ -8,13 +8,35 @@ import os
 from django.conf import settings
 
 
+def age_gender_detection(file_path):  # временная заглушка
+    min_age = 0
+    max_age = 100
+    gender = 'Male'
+    return min_age, max_age, gender
+
+
 class ImageAPI(APIView):
     def get(self, request):
-        file = request.FILES['photo']
-        return JsonResponse({'Status': 'Error', 'Detail': 'Use POST', 'File name': str(file)})
+        try:
+            file = request.FILES['photo']
+            return JsonResponse({
+                'status': 'Error',
+                'detail': 'Use POST',
+                'file_name': str(file)})
+        except (KeyError, OSError) as e:
+            return JsonResponse({'Status': 'Error', 'Detail': 'KeyError of wrong file'})
 
     def post(self, request):
-        file = request.FILES['photo']
-        path = default_storage.save(str(file), ContentFile(file.read()))
-        tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-        return JsonResponse({'Status': 'success', 'Image name': str(file)})
+        try:
+            file = request.FILES['photo']
+            path = default_storage.save(str(file), ContentFile(file.read()))
+            tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+            min_age, max_age, gender = age_gender_detection(tmp_file)
+            return JsonResponse({
+                'status': 'success',
+                'min_age': min_age,
+                'max_age': max_age,
+                'gender': gender
+            })
+        except (KeyError, OSError) as e:
+            return JsonResponse({'Status': 'Error', 'Detail': 'KeyError or wrong file'})
