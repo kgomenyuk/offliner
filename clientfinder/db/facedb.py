53a0c9ad46114e0726e7e6371bedcdebfcbb14cc
Fedor <fmleonov@edu.hse.ru>
2020-09-02 16:07:37 +0300

    added class for db insertion

---
+++ clientfinder/db/facedb.py
@@ -0,0 +1,35 @@
+import psycopg2
+import os
+
+
+class get_db:
+    def __init__(self):
+        self.name = os.environ.get('DB_NAME')
+        self.user = os.environ.get('DB_USER')
+        self.password = os.environ.get('DB_PWD')
+        self.host = os.environ.get('DB_HOST')
+        self.port = os.environ.get('DB_PORT')
+
+    def insert(self, data):
+        self.conn = psycopg2.connect(database=self.name, user=self.user, password=self.password, host=self.host,
+                                     port=self.port)
+        self.cur = self.conn.cursor()
+        for i in data:
+            self.cur.execute(f"INSERT INTO FACEVECTORS (FACEVECTORS) VALUES ({i})")
+        self.conn.commit()
+        print('Inserted')
+        self.cur.close()
+        self.conn.close()
+        print('Cursor and connection Closed')
+
+    def get(self):
+        self.conn = psycopg2.connect(database=self.name, user=self.user, password=self.password, host=self.host,
+                                     port=self.port)
+        self.cur = self.conn.cursor()
+        self.cur.execute("SELECT FACEVECTORS FROM FACEVECTORS")
+        rows = self.cur.fetchall()
+        print("Got: ")
+        self.cur.close()
+        self.conn.close()
+        print('Cursor and connection Closed')
+        return rows
