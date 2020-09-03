b719127c16eda9793d719e917b0b2fff62dbedf5
Danes <notdanes@gmail.com>
2020-09-03 19:06:31 +0300

    knn w/ time implemented
    RecClientCatWithTime:
    +postgressql knn
    +simple hours getting algorithm
    -sorting algo with hard time zones edges

--- recommender/src/recomendation_system/RecClientCatWithTime.ts
+++ recommender/src/recomendation_system/RecClientCatWithTime.ts
@@ -2,38 +2,31 @@ import { RecAlgo } from "./RecAlgo";
 import { RecResult } from "./RecResult";
 import { Client } from "./Client";
 import { Client as postgresClient}  from "ts-postgres";
+import { forEach } from "async";
+import { time } from "console";
+import { cli } from "winston/lib/winston/config";
 
 export class RecClientCatWithTime extends RecAlgo{
     async run(client: Client): Promise<Array<RecResult>> {
         const arrayResult = new Array<RecResult>();
         const db = new postgresClient(this.dbConfig);
-        const startLunch = 12;
-        const startDinner = 17;
+
+        const timeSchedule = [9, 15, 21]
+
+
         await db.connect();
         try {
-            let forbreakfast = 0;
-            let forlunch = 0;
-            let fordiner = 0;
-
-            if (client.timeHours.getHours() >= startLunch || client.timeHours.getHours() <= startDinner){
-                forlunch = 1;
-            }
-            else if (client.timeHours.getHours() >= startDinner){
-                fordiner = 1;
-            }
-            else{
-                forbreakfast = 1;
+            let curPeriod: number = 0;
+            for await (const timestamp of timeSchedule) {
+                if (new Date().getHours() - timestamp < 0) { break; }
+                else { curPeriod = timestamp; }
             }
 
-            const sql = `SELECT position_id::text, (row_number() OVER(ORDER BY agemin-$2::int asc, count(*) DESC))::int as place,
-            forbreakfast::int, forlunch::int, fordinner::int
-            FROM contents WHERE gender = $1::text AND agemin BETWEEN $2::int AND $3::text = 1
-            AND forbreakfast = $4::int AND forlunch = $5::int AND fordinner = $6::int}
-            group by position_id, agemin
-            ORDER BY place asc
-            limit 20`;
-            
-            const resultIterator = db.query(sql, [client.gender, client.minAge, client.maxAge, forbreakfast, forlunch, fordiner]);
+            const sql = `SELECT position_id::text, agemin::int, agemax::int, order_date::date from contents
+	        order by gender <-> $1::text, cube(agemin,agemax) <-> cube($2::int,$3::int)::cube asc, cube(extract(hour from now())) <-> cube($5), abs(order_date::date-now()::date) asc, abs(order_day_of_week::int-$4) asc
+	        limit 20`;
+ 
+            const resultIterator = db.query(sql, [client.gender, client.minAge, client.maxAge, new Date().getDay(), curPeriod]);
             
             for await (const row of resultIterator) {
                 const result = new RecResult();
